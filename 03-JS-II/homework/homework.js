2cd1dc22ce336ea38310e86854414230ff8d0bbe
Dani <santifernandez93s@gmail.com>
2022-01-26 13:47:11 -0300

    updated homework JSII

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,6 +5,7 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x===y) return x || y;
   if (x > y) {
     return x;
   } return y;
